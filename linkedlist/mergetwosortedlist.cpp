76bb55ddc6c9b6d9a194c597f4ab5cc92c7e07c6
rishi <128016062+sharmarishikesh1215@users.noreply.github.com>
2025-08-10 17:36:41 +0000

    added merge two sorted list code

---
+++ linkedlist/mergetwosortedlist.cpp
@@ -0,0 +1,110 @@
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+*/
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Node class represents a
+// node in a linked list
+class Node {
+public:
+    // Data stored in the node
+    int data;
+    
+    // Pointer to the next node in the list
+    Node* next;
+
+    // Constructor with both data and
+    // next node as parameters
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
+
+    // Constructor with only data as a
+    // parameter, sets next to nullptr
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+// Function to merge two sorted linked lists
+Node* sortTwoLinkedLists(Node* list1, Node* list2) {
+    // Create a dummy node to serve
+    // as the head of the merged list
+    Node* dummyNode = new Node(-1);
+    Node* temp = dummyNode;
+
+    // Traverse both lists simultaneously
+    while (list1 != nullptr && list2 != nullptr) {
+        // Compare elements of both lists and
+        // link the smaller node to the merged list
+        if (list1->data <= list2->data) {
+            temp->next = list1;
+            list1 = list1->next;
+        } else {
+            temp->next = list2;
+            list2 = list2->next;
+        }
+        // Move the temporary pointer
+        // to the next node
+        temp = temp->next; 
+    }
+
+    // If any list still has remaining
+    // elements, append them to the merged list
+    if (list1 != nullptr) {
+        temp->next = list1;
+    } else {
+        temp->next = list2;
+    }
+    // Return the merged list starting 
+    // from the next of the dummy node
+    return dummyNode->next;
+}
+
+// Function to print the linked list
+void printLinkedList(Node* head) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        // Print the data of the current node
+        cout << temp->data << " "; 
+        // Move to the next node
+        temp = temp->next; 
+    }
+    cout << endl;
+}
+
+int main() {
+    // Example Linked Lists
+    Node* list1 = new Node(1);
+    list1->next = new Node(3);
+    list1->next->next = new Node(5);
+
+    Node* list2 = new Node(2);
+    list2->next = new Node(4);
+    list2->next->next = new Node(6);
+
+    cout << "First sorted linked list: ";
+    printLinkedList(list1);
+
+    cout << "Second sorted linked list: ";
+    printLinkedList(list2);
+
+    Node* mergedList = sortTwoLinkedLists(list1, list2);
+
+    cout << "Merged sorted linked list: ";
+    printLinkedList(mergedList);
+
+    return 0;
+}
